44ceec3e71ecbd2794616887a98d91cd2071822f
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-04-18 19:27:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def turn_count(board)
+def turn_count(board, default = "X")
   gameplay = ["Turn 1", "Turn 2", "Turn 3", "Turn 4", "Turn 5", "Turn 6", "Turn 7", "Turn 8", "Turn 9"]
   counter = 0
   gameplay.each do |turn|
