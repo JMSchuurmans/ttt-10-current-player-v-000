140af4e6699eac3dbef40564a7b2b8028c25314a
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-04-18 19:15:56 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count
-  gameplay = {rn 1, turn 2, turn 3, turn 4, turn 5, turn 6, turn 7, turn 8, turn 9}
+  gameplay = {turn 1, turn 2, turn 3, turn 4, turn 5, turn 6, turn 7, turn 8, turn 9}
   counter = 0
   gameplay.each do |turn|
     puts "It is now #{turn}"
